7d1ea8abfc85695dd9a8cf1b30c295dd9b85c10f
sahalya521 <sahalya06@gmail.com>
2025-09-10 14:36:23 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition {
+    public static void main(String[] args) {
+        int num1 = 5; // First number
+        int num2 = 7; // Second number
+
+        int sum = num1 + num2; // Addition operation
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
